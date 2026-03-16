ce7c3134fe7dbb8d80d8c5aebc6ad3fee260b64a
vararais <varadigmaisnann@gmail.com>
2026-03-16 20:56:42 +0700

    Menambah fungsi Rest controller dan kelas exception handler

--- src/main/java/com/example/praktikum2/util/GlobalExceptionHandler.java
+++ src/main/java/com/example/praktikum2/util/GlobalExceptionHandler.java
@@ -1,4 +1,19 @@
 package com.example.praktikum2.util;
 
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-}
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationException(ConstraintViolationException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of(
+                "status", "error",
+                "message", exception.getMessage()
+        ));
+    }
