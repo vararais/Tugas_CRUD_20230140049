e67ebadedefc77882929c55a6309b27babc2b4ff
vararais <varadigmaisnann@gmail.com>
2026-03-16 20:55:39 +0700

    Menambah file validasi util

---
+++ src/main/java/com/example/praktikum2/util/ValidationUtil.java
@@ -0,0 +1,24 @@
+package com.example.praktikum2.util;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import java.util.Set;
+
+@Component
+@AllArgsConstructor
+public class ValidationUtil {
+
+    @Autowired
+    private Validator validator;
+
+    public void validate(Object request) {
+        Set<ConstraintViolation<Object>> constraintViolations = validator.validate(request);
+        if (!constraintViolations.isEmpty()) {
+            throw new ConstraintViolationException(constraintViolations);
+        }
+    }
+}
