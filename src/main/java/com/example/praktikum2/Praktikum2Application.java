0614d9088552d798d2940232822117605fa6c2e7
vararais <varadigmaisnann@gmail.com>
2026-03-16 20:37:48 +0700

    Initial commit: Setup project structure and dependencies

---
+++ src/main/java/com/example/praktikum2/Praktikum2Application.java
@@ -0,0 +1,13 @@
+package com.example.praktikum2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praktikum2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Praktikum2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/praktikum2/Praktikum2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praktikum2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktikum2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
