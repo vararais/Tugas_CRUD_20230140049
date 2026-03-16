ee923870056173c7350d806116e26fec33227451
vararais <varadigmaisnann@gmail.com>
2026-03-16 21:08:15 +0700

    Menambah anotasi pada model entity

---
+++ src/main/java/com/example/praktikum2/model/entity/Ktp.java
@@ -0,0 +1,12 @@
+package com.example.praktikum2.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "ktp")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
